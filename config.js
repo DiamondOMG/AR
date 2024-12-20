b27576a5befdb3890ece3bbc31118a042ca66c85
DiamondOMG <omgdigital.developer.02@gmail.com>
2024-12-20 17:06:31 +0700

    Vite

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  SUPABASE_BASE_URL: "https://rdzvmchyqrewpkeccasd.supabase.co/rest/v1",
+  SUPABASE_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkenZtY2h5cXJld3BrZWNjYXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ1ODkwNTYsImV4cCI6MjA1MDE2NTA1Nn0._oeU8LF8fCwDu3YiyIlqH7e0PJN0gOvqAbNDIW8Gzqg",
+};
+
+// ต้องมี export default
+export default config;
