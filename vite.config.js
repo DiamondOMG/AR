0c96cdc94daca6af63218cf9320d4d811c4a40f7
Natnatee <natnatee_mond@hotmail.com>
2024-12-21 16:38:58 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 /** @type {import('vite').UserConfig} */
 export default {
-  build: {
-    rollupOptions: {
-      input: ["index.html", "game.html"],
-    },
-    assetsInclude: ["**/*.patt", "**/*.gltf", "**/*.bin", "**/*.glb"],
-  },
-  base: "/",
-  publicDir: "public",
+	build: {
+		rollupOptions: {
+			input: {
+				index: "index.html",
+				game: "game.html",
+			},
+		},
+		assetsInclude: ["**/*.patt", "**/*.gltf", "**/*.bin", "**/*.glb"],
+	},
+	base: "/",
+	publicDir: "public",
 };
